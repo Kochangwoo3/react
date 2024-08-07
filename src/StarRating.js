f4a857b7cdc48abea4da3917336322364da393f5
Kochangwoo3 <love134@bible.ac.kr>
2024-08-07 18:38:37 +0900

    Add files via upload

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
-import Star from "./Star";
+import React from "react";
 import { createArray } from "./lib";
+import Star from "./Star";
 
-// function handleStarSelect (index, setSelectedStars) {
-//   return setSelectedStars(index + 1);
-// }
-
-export default function StarRating({ totalStars = 5} ){
-  const [selectedStars, setSelectedStars] = useState(0);
-
+export default function StarRating({ 
+  totalStars=5, 
+  selectedStars = 0,
+  onRate = f=>f
+}){
   return (
     <>
       {createArray(totalStars).map((n, i) => (
-          <Star
-            key= {i}
-            selected={selectedStars > i}     
-            // selectedStars:선택값 [1], 2, 3, 4, 5  i=> 0, 1, 2, 3, 4   
-            //onSelect={() => handleStarSelect(i, setSelectedStars)}
-            onSelect={() => setSelectedStars(i+1)} 
-            />
-      )) }
+        <Star 
+          key={i} 
+          selected={selectedStars > i} 
+          onSelect={()=> onRate(i+1)}
+        />
+      ))}
       <p>
         {selectedStars} of {totalStars} stars
       </p>
