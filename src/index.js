7c850e0121755053c2b75ea9bf6ae2f99314f59f
Kochangwoo3 <love134@bible.ac.kr>
2024-08-09 18:28:11 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import App01 from './App01';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App01 />
+    <App />
 );
-
