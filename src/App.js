0f5f67b5f842da2cd5db50b70e7f96cd69d77473
Kochangwoo3 <love134@bible.ac.kr>
2024-08-16 18:31:05 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy, useState } from 'react';
+import Agreement from './Agreement';
+import { ClimbingBoxLoader } from 'react-spinners';
+
+const Main = lazy(() => import('./Main.js'));
 
 function App() {
+  const [agree, setAgree] = useState(false);
+
+  if (!agree) return <Agreement onAgree={() => setAgree(true)} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={<ClimbingBoxLoader />}>
+        <Main />
+      </Suspense>
     </div>
   );
 }
