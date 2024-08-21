1f9e7391d6842210d54aea7b74f739b7ad4c81fe
Kochangwoo3 <love134@bible.ac.kr>
2024-08-21 10:41:32 +0900

    Delete src directory

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('learn react link가 화면에 표시되나요?', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function getNews({ order = 'aid', offset = 0, limit = 6 }) {
-  const queryString = `order=${order}&offset=${offset}&limit=${limit}`;
-
-  const response = await fetch(`http://localhost:8090/api/news?${queryString}`);
-  const body = await response.json();
-  return body;
-}
