58a4176e79af96a03a6455557ffeaee0d9c1074c
Kochangwoo3 <love134@bible.ac.kr>
2024-08-21 12:03:46 +0900

    Add files via upload

---
+++ chapter-06-1/01_star-rating/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StarRating from "./StarRating";
+
+function App() {
+  return (
+    <StarRating 
+      style={ { backgroundColor: "lightblue"} } 
+      onDoubleClick={e => alert("double click")}
+    />
+  );
+}
+
+export default App;
---
+++ chapter-06-1/01_star-rating/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import StarRating from "./StarRating";
+
+function App() {
+  return (
+    <StarRating />
+  );
+}
+
+export default App;
